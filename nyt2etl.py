ce03f2709aea19f54046abfa570c302a734e1fc5
thu_h <thu_ha_hong_tech@outlook.com>
2021-03-01 15:42:26 +0000

    pulling

--- nyt2etl.py
+++ nyt2etl.py
@@ -1,4 +1,5 @@
 import csv, json
+from datetime import datetime
 
 class Etl:
     def __init__(self):
@@ -7,6 +8,9 @@ class Etl:
         self.read_json = []
         self.new_columns = ''
         self.csv_format = [[]]
+        self.datetime = ''
+        self.timestamp = ''
+
     def extract(self):
         with open(self.json_file, encoding='utf-8') as file:
             for line in file:
@@ -23,6 +27,16 @@ class Etl:
         return self.new_columns
 
     def change_data_format(self):
+        t = [1211587200000, 1212192000000, 1212796800000]
+        for book in self.new_columns:
+            timesstamp = book[1]
+            date = self.datetime(timestamp / le3)
+        print(self.timestamp)
+
+#your_timestamp = 1331856000000
+#date = datetime.datetime.fromtimestamp(your_timestamp / 1e3)
+
+
         return self.new_columns
 
     def json_to_csv(self):
@@ -30,11 +44,12 @@ class Etl:
 
     def load_csv(self):
         pass
-    
+
     def main(self, old_file_name):
         self.json_file = old_file_name
         self.extract()
-        print(self.remove_bestseller_column())
+        self.remove_bestseller_column()
+        self.change_data_format()
         pass
 
 instance = Etl()
