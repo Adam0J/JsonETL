8734685a4cdb44915a95bbd9362db701bee00662
Tyler Hulbert <THulbert@spartaglobal.com>
2021-03-01 13:40:39 +0000

    Start of date format

--- nyt2etl.py
+++ nyt2etl.py
@@ -1,5 +1,5 @@
 import csv, json
-
+from datetime import datetime
 
 class Etl:
     def __init__(self):
@@ -8,6 +8,7 @@ class Etl:
         self.read_json = []
         self.new_columns = ''
         self.csv_format = [[]]
+        self.datetime = ''
 
     def extract(self):
             with open(self.json_file, encoding='utf-8') as file:
@@ -21,14 +22,17 @@ class Etl:
         return self.read_json
 
     def remove_bestseller_column(self):
-
         for i in self.read_json:
             del i["bestsellers_date"]
         self.new_columns = self.read_json
         return self.new_columns
 
     def change_data_format(self):
-
+        t = [1212883200000, 1214092800000, 1214697600000]
+        for book in t:
+            #timestamp = book[1]
+            date = datetime.fromtimestamp(book / 1e3)
+            print(date)
         return self.new_columns
 
     def json_to_csv(self):
@@ -43,6 +47,7 @@ class Etl:
         self.json_file = old_file_name
         self.extract()
         self.remove_bestseller_column()
+        self.change_data_format()
         pass
 
 instance = Etl()
