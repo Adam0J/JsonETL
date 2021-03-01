e2fbc433799b43ebe1326269d8e9f7f62af122c7
Reece Penn <reecepenn97@gmail.com>
2021-03-01 13:53:31 +0000

    Scrum work

---
+++ new_nyt2etl.py
@@ -0,0 +1,58 @@
+import csv, json
+
+
+class Etl:
+    def __init__(self):
+        self.json_file = "nyt2.json"
+        self.new_csv_file = ''
+        self.read_json = []
+        self.new_columns = ''
+        self.csv_format = [[]]
+
+    def extract(self):
+        with open(self.json_file, "r", encoding='utf-8') as file:
+            for line in file:
+                self.read_json.append(json.loads(line))
+            #print(self.read_json)
+        return self.read_json
+
+    def remove_data_types(self):
+
+        keys = list(self.read_json[0].keys())
+        print(keys)
+        for dictionary in self.read_json:
+            for key in keys:
+                values = dictionary[key]
+                if isinstance(values,dict):
+                    values = list(values.values())[0]
+                    if isinstance(values,dict):
+                        values = list(values.values())[0]
+                        dictionary[key] = values
+                    else:
+                        dictionary[key] = values
+        print(self.read_json)
+
+    def remove_bestseller_column(self):
+
+        return self.new_columns
+
+    def change_data_format(self):
+
+        return self.new_columns
+
+    def json_to_csv(self):
+
+        return self.csv_format
+
+    def load_csv(self):
+
+        pass
+
+    def main(self):
+
+        pass
+
+test = Etl()
+test.extract()
+test.remove_data_types()
+
