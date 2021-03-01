db6106bbc702826da29662f4b9234638b5bddb7e
Victor Novoa <victorhelionm@gmail.com>
2021-03-01 14:55:35 +0000

    load_csv completed

--- nyt2etl.py
+++ nyt2etl.py
@@ -4,7 +4,7 @@ import csv, json
 class Etl:
     def __init__(self):
         self.json_file = "nyt2.json"
-        self.new_csv_file = ''
+        self.new_csv_file = 'nyt2.csv'
         self.read_json = []
         self.new_columns = ''
         self.csv_format = [[]]
@@ -17,9 +17,8 @@ class Etl:
         return self.read_json
 
     def remove_data_types(self):
-
         keys = list(self.read_json[0].keys())
-        print(keys)
+        #print(keys)
         for dictionary in self.read_json:
             for key in keys:
                 values = dictionary[key]
@@ -30,7 +29,16 @@ class Etl:
                         dictionary[key] = values
                     else:
                         dictionary[key] = values
-        print(self.read_json)
+        return self.read_json
+
+    def transform_data(self):
+        for dictionary in self.read_json:
+            for key in dictionary:
+                if key == 'price':
+                    dictionary[key] = round(float(dictionary[key]),2)
+                elif key == 'rank' or key == 'rank_last_week' or key == 'weeks_on_list' or key == 'published_date':
+                    dictionary[key] = int(dictionary[key])
+        return self.read_json
 
     def remove_bestseller_column(self):
 
@@ -42,11 +50,16 @@ class Etl:
 
     def json_to_csv(self):
 
+        for x in self.read_json[0].keys():
+            self.csv_format[0].append(x)
+        for dictionary in self.read_json:
+                self.csv_format.append(list(dictionary.values()))
         return self.csv_format
 
     def load_csv(self):
-
-        pass
+        with open(self.new_csv_file, "w",newline="", encoding='utf-8') as new_file:
+            csv_writer = csv.writer(new_file)
+            csv_writer.writerows(self.csv_format)
 
     def main(self):
 
@@ -55,3 +68,6 @@ class Etl:
 test = Etl()
 test.extract()
 test.remove_data_types()
+test.transform_data()
+test.json_to_csv()
+test.load_csv()
